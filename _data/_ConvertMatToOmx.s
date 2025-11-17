330b04ac6f2d777d949cf2029917a67ed817e6fd
Pukar Bhandari <pukar.bhandari@wfrc.utah.gov>
2025-11-17 13:35:23 -0700

    move the crt trip distribution to this repo

---
+++ _data/_ConvertMatToOmx.s
@@ -0,0 +1,12 @@
+;script used to convert MTX or MAT files to OMX format
+;no module needed because it runs in PILOT
+;more info here: https://bentleysystems.service-now.com/community?id=kb_article_view&sysparm_article=KB0102026
+
+convertmat from = "Skims/skm_d8_Ok.mtx" to = "Skims/omx/skm_d8_Ok.omx" compression = 1 format = omx
+convertmat from = "Skims/skm_d8_Pk.mtx" to = "Skims/omx/skm_d8_Pk.omx" compression = 1 format = omx
+
+convertmat from = "Skims/skm_w8_flag_Ok.mtx" to = "Skims/omx/skm_w8_flag_Ok.omx" compression = 1 format = omx
+convertmat from = "Skims/skm_w8_flag_Pk.mtx" to = "Skims/omx/skm_w8_flag_Pk.omx" compression = 1 format = omx
+
+convertmat from = "Skims/skm_w8_Ok.mtx" to = "Skims/omx/skm_w8_Ok.omx" compression = 1 format = omx
+convertmat from = "Skims/skm_w8_Pk.mtx" to = "Skims/omx/skm_w8_Pk.omx" compression = 1 format = omx
