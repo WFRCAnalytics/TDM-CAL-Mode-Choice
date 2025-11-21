410dd0dab323d2965c0e405738da119cafe9fb54
Pukar Bhandari <pukar.bhandari@wfrc.utah.gov>
2025-11-21 11:54:33 -0700

    render using revised matrices

--- _data/_ConvertMatToOmx.s
+++ _data/_ConvertMatToOmx.s
@@ -2,11 +2,11 @@
 ;no module needed because it runs in PILOT
 ;more info here: https://bentleysystems.service-now.com/community?id=kb_article_view&sysparm_article=KB0102026
 
-convertmat from = "Skims/original/skm_d8_Ok.mtx" to = "Skims/omx/skm_d8_Ok.omx" compression = 1 format = omx
-convertmat from = "Skims/original/skm_d8_Pk.mtx" to = "Skims/omx/skm_d8_Pk.omx" compression = 1 format = omx
+convertmat from = "Skims/revised/skm_d8_Ok.mtx" to = "Skims/omx/skm_d8_Ok.omx" compression = 1 format = omx
+convertmat from = "Skims/revised/skm_d8_Pk.mtx" to = "Skims/omx/skm_d8_Pk.omx" compression = 1 format = omx
 
-convertmat from = "Skims/original/skm_w8_flag_Ok.mtx" to = "Skims/omx/skm_w8_flag_Ok.omx" compression = 1 format = omx
-convertmat from = "Skims/original/skm_w8_flag_Pk.mtx" to = "Skims/omx/skm_w8_flag_Pk.omx" compression = 1 format = omx
+convertmat from = "Skims/revised/skm_w8_flag_Ok.mtx" to = "Skims/omx/skm_w8_flag_Ok.omx" compression = 1 format = omx
+convertmat from = "Skims/revised/skm_w8_flag_Pk.mtx" to = "Skims/omx/skm_w8_flag_Pk.omx" compression = 1 format = omx
 
-convertmat from = "Skims/original/skm_w8_Ok.mtx" to = "Skims/omx/skm_w8_Ok.omx" compression = 1 format = omx
-convertmat from = "Skims/original/skm_w8_Pk.mtx" to = "Skims/omx/skm_w8_Pk.omx" compression = 1 format = omx
+convertmat from = "Skims/revised/skm_w8_Ok.mtx" to = "Skims/omx/skm_w8_Ok.omx" compression = 1 format = omx
+convertmat from = "Skims/revised/skm_w8_Pk.mtx" to = "Skims/omx/skm_w8_Pk.omx" compression = 1 format = omx
